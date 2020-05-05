dcb9395912a6189cc4150168da479bfb315fb875
Céline Cottier <cottier.celine@gmail.com>
2020-05-05 16:54:34 +0200

    first exercise solved and second started

--- src/App.js
+++ src/App.js
@@ -7,12 +7,56 @@ const initialProductList = [
   { id: 3, name: 'produit 3', price: 20, quantity: 5 }
 ];
 
-function App () {
-  return (
+class App extends React.Component {
+  
+  state = {initialProductList}
+
+
+  handleChange = (e) => {
+    return e.target.value
+    // console.log(e.target.value)
+    // console.log(this.state.initialProductList)
+    // console.log(...this.state.initialProductList)
+    // console.log({ ...this.state.initialProductList[id], quantity : e.target.value})
+    // this.setState({ initialProductList: { ...this.state.initialProductList, quantity : e.target.value}})
+  }
+
+  changement = (id) => {
+    console.log({ ...this.state.initialProductList[id], quantity : this.handleChange})
+    // this.setState({ initialProductList: { ...this.state.initialProductList, quantity : this.handleChange}})
+  }
+
+  
+
+  render(){return (
     <div className='App'>
       <h1>Ma commande</h1>
+      <table>
+        <thead>
+            <tr>
+                <th>Produit</th>
+                <th>Prix unitaire</th>
+                <th>Quantité</th>
+                <th>Prix total</th>
+            </tr>
+        </thead>
+        <tbody>
+        {this.state.initialProductList.map (product => (
+          <tr key={product.id}>
+              <td>{product.name}</td>
+              <td>{product.price}</td>
+              <td><input type="number" value={product.quantity} onChange={this.changement}/></td>
+              <td>{product.quantity * product.price} </td>
+          </tr>
+            ))}
+
+        </tbody>
+</table>
     </div>
-  );
+  );}
+  
 }
 
 export default App;
+
+
